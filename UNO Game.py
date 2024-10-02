02063cd3ade81976c529a90cd4604eb6e945939e
SpudGunMan <kellykeeton@yahoo.com>
2024-10-02 00:01:11 -0700

    multiple player

--- UNO Game.py
+++ UNO Game.py
@@ -1,15 +1,26 @@
 import random
 import time
 
+color = ('RED', 'GREEN', 'BLUE', 'YELLOW')
+rank = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw2', 'Draw4', 'Wild')
+ctype = {'0': 'number', '1': 'number', '2': 'number', '3': 'number', '4': 'number', '5': 'number', '6': 'number',
+         '7': 'number', '8': 'number', '9': 'number', 'Skip': 'action', 'Reverse': 'action', 'Draw2': 'action',
+         'Draw4': 'action_nocolor', 'Wild': 'action_nocolor'}
 
-color = ('RED','GREEN','BLUE','YELLOW')
-rank = ('0','1','2','3','4','5','6','7','8','9','Skip','Reverse','Draw2','Draw4','Wild')
-ctype = {'0':'number','1':'number','2':'number','3':'number','4':'number','5':'number','6':'number',
-            '7':'number','8':'number','9':'number','Skip':'action','Reverse':'action','Draw2':'action',
-            'Draw4':'action_nocolor','Wild':'action_nocolor'}
+# Player List
+playerListUno = ['🤖Bot', ]
+playWithBot = input('Do you want to play with the Uno Bot? (y/n): ').lower()
+num_players = int(input('Enter number of players (including you): '))
+for i in range(num_players):
+    playerListUno.append(input(f'Enter Player {i + 1} name: '))
+if playWithBot == 'n' and num_players >= 2:
+    playerListUno.remove('🤖Bot')
+
+print('\nPlayers in the game:')
+for i in range(len(playerListUno)):
+    print(f'{i + 1}. {playerListUno[i]}')
 
 class Card:
-
     def __init__(self, color, rank):
         self.rank = rank
         if ctype[rank] == 'number':
@@ -23,14 +34,12 @@ class Card:
             self.cardtype = 'action_nocolor'
 
     def __str__(self):
-        if self.color == None:
+        if self.color is None:
             return self.rank
         else:
             return self.color + " " + self.rank
 
-
 class Deck:
-
     def __init__(self):
         self.deck = []
         for clr in color:
@@ -53,9 +62,7 @@ class Deck:
     def deal(self):
         return self.deck.pop()
 
-
 class Hand:
-
     def __init__(self):
         self.cards = []
         self.cardsstr = []
@@ -91,65 +98,59 @@ class Hand:
             int(card.rank) if card.cardtype == 'number' and card.rank is not None else 0))
         self.cardsstr = [str(card) for card in self.cards]
 
-
-#Funciton to randomly select who starts first
 def choose_first():
-    if random.randint(0,1)==0:
-        return 'Player'
-    else:
-        return 'Pc'
+    random_player = random.choice(playerListUno)
+    return random_player
 
-
-#Function to check if the card thrown by Player/PC is a valid card by comparing it with the top card
-def single_card_check(top_card,card):
-    if card.color==top_card.color or top_card.rank==card.rank or card.cardtype=='action_nocolor':
+def single_card_check(top_card, card):
+    special_rank = ('Wild', 'Draw4', 'Draw2', 'Skip', 'Reverse')
+    if top_card.rank in special_rank:
+        return True
+    if card.color == top_card.color or top_card.rank == card.rank or card.cardtype == 'action_nocolor':
         return True
     else:
         return False
 
-
-#FOR PC ONLY
-#To check if PC has any valid card to throw 
-def full_hand_check(hand,top_card):
+def full_hand_check(hand, top_card):
     for c in hand.cards:
-        if c.color==top_card.color or c.rank == top_card.rank or c.cardtype=='action_nocolor':
-            return hand.remove_card(hand.cardsstr.index(str(c))+1)
+        if c.color == top_card.color or c.rank == top_card.rank or c.cardtype == 'action_nocolor':
+            return hand.remove_card(hand.cardsstr.index(str(c)) + 1)
     else:
         return 'no card'
 
-
-#Function to check if either wins
 def win_check(hand):
-    if len(hand.cards)==0:
+    if len(hand.cards) == 0:
         return True
     else:
         return False
 
-
-#Function to check if last card is an action card (GAME MUST END WITH A NUMBER CARD)
 def last_card_check(hand):
     for c in hand.cards:
-        if c.cardtype!='number':
+        if c.cardtype != 'number':
             return True
         else:
             return False
 
+def getNextPlayer(playerIndex, direction=1, skip=False):
+    current_index = playerListUno.index(playerIndex)
+    next_index = (current_index + direction) % len(playerListUno)
+    if skip:
+        next_index = (next_index + direction) % len(playerListUno)
+    return playerListUno[next_index]
 
-#The gaming loop
+def getCurrentPlayer(playerIndex):
+    return playerIndex
+
 while True:
-
     print('Welcome to UNO! Finish your cards first to win')
 
     deck = Deck()
     deck.shuffle()
 
-    player_hand = Hand()
-    for i in range(7):
-        player_hand.add_card(deck.deal())
-
-    pc_hand = Hand()
-    for i in range(7):
-        pc_hand.add_card(deck.deal())
+    hands = {player: Hand() for player in playerListUno}
+    for player in playerListUno:
+        for i in range(7):
+            hands[player].add_card(deck.deal())
 
     top_card = deck.deal()
     if top_card.cardtype != 'number':
@@ -160,73 +161,75 @@ while True:
     playing = True
 
     turn = choose_first()
-    print(turn + ' will go first')
+    print('\n' + turn + ' goes first')
+    direction = 1
 
     while playing:
-
-        if turn == 'Player':
+        player = getCurrentPlayer(turn)
+        print('\n' + player + "'s turn")
+        if player != '🤖Bot':
             print('\nTop card is: ' + str(top_card))
             print('\nYour cards: ')
-            player_hand.cards_in_hand()
-            # Check if card in hand is playable
+            hands[player].cards_in_hand()
             playableHand = False
-            for i in range (len(player_hand.cardsstr)):
-                if single_card_check(top_card, player_hand.single_card(i)):
+            for i in range(len(hands[player].cardsstr)):
+                if single_card_check(top_card, hands[player].single_card(i + 1)):
                     playableHand = True
 
-            if player_hand.no_of_cards() == 1:
-                print('Player yells UNO!!')
+            if hands[player].no_of_cards() == 1:
+                print('{} Yells UNO!!'.format(player))
             if not playableHand:
-                print('You dont have any playable card, drawing from deck')
+                print('{} doesn\'t have a card'.format(player))
                 choice = 'd'
-            else: 
+            else:
                 choice = input("\nHit/Card# or Pull/Draw? (h/p): ")
             if choice.lower().startswith('h') or choice.isnumeric():
                 if choice.isnumeric():
                     pos = int(choice)
                 else:
                     pos = int(input('Enter index of card: '))
-                temp_card = player_hand.single_card(pos)
+                temp_card = hands[player].single_card(pos)
                 if single_card_check(top_card, temp_card):
                     if temp_card.cardtype == 'number':
-                        top_card = player_hand.remove_card(pos)
-                        turn = 'Pc'
+                        top_card = hands[player].remove_card(pos)
+                        turn = getNextPlayer(player, direction)
                     else:
                         if temp_card.rank == 'Skip':
-                            turn = 'Player'
-                            top_card = player_hand.remove_card(pos)
+                            turn = getNextPlayer(player, direction, skip=True)
+                            top_card = hands[player].remove_card(pos)
                         elif temp_card.rank == 'Reverse':
-                            turn = 'Player'
-                            top_card = player_hand.remove_card(pos)
+                            direction *= -1
+                            turn = getNextPlayer(player, direction)
+                            top_card = hands[player].remove_card(pos)
                         elif temp_card.rank == 'Draw2':
-                            pc_hand.add_card(deck.deal())
-                            pc_hand.add_card(deck.deal())
-                            top_card = player_hand.remove_card(pos)
-                            turn = 'Pc'
+                            next_player = getNextPlayer(player, direction)
+                            hands[next_player].add_card(deck.deal())
+                            hands[next_player].add_card(deck.deal())
+                            top_card = hands[player].remove_card(pos)
+                            turn = getNextPlayer(player, direction)
                         elif temp_card.rank == 'Draw4':
+                            next_player = getNextPlayer(player, direction)
                             for i in range(4):
-                                pc_hand.add_card(deck.deal())
-                            top_card = player_hand.remove_card(pos)
+                                hands[next_player].add_card(deck.deal())
+                            top_card = hands[player].remove_card(pos)
                             draw4color = input('Change color to: ')
                             if draw4color != draw4color.upper():
                                 draw4color = draw4color.upper()
-                            # assume single character first letter of color
                             if draw4color[0] in [c[0] for c in color]:
                                 draw4color = [c for c in color if c[0] == draw4color[0]][0]
                                 print('Color changes to', draw4color)
                             top_card.color = draw4color
-                            turn = 'Pc'
+                            turn = getNextPlayer(player, direction)
                         elif temp_card.rank == 'Wild':
-                            top_card = player_hand.remove_card(pos)
+                            top_card = hands[player].remove_card(pos)
                             wildcolor = input('Change color to: ')
                             if wildcolor != wildcolor.upper():
                                 wildcolor = wildcolor.upper()
-                            # assume single character first letter of color
                             if wildcolor[0] in [c[0] for c in color]:
                                 wildcolor = [c for c in color if c[0] == wildcolor[0]][0]
                                 print('Color changes to', wildcolor)
                             top_card.color = wildcolor
-                            turn = 'Pc'
+                            turn = getNextPlayer(player, direction)
                 else:
                     print('{} cannot be used on {}'.format(temp_card, top_card))
             elif choice.lower().startswith('p') or choice.lower().startswith('d'):
@@ -234,55 +237,60 @@ while True:
                 print('You got: ' + str(temp_card))
                 time.sleep(1)
                 if single_card_check(top_card, temp_card):
-                    player_hand.add_card(temp_card)
+                    hands[player].add_card(temp_card)
                 else:
                     print('Cannot use this card')
-                    player_hand.add_card(temp_card)
-                    turn = 'Pc'
-            if win_check(player_hand):
-                print('\nPLAYER WON!!')
+                    hands[player].add_card(temp_card)
+                    turn = getNextPlayer(player, direction)
+            if win_check(hands[player]):
+                print('\n{} WON!!'.format(player))
                 playing = False
                 break
-
-        if turn == 'Pc':
-            temp_card = full_hand_check(pc_hand, top_card)
+        
+        print('{} has {} cards remaining'.format(player, hands[player].no_of_cards()))
+        # Bot Logic
+        if player == '🤖Bot':
+            temp_card = full_hand_check(hands[player], top_card)
             time.sleep(1)
             if temp_card != 'no card':
                 print(f'\nPC throws: {temp_card}')
                 time.sleep(1)
                 if temp_card.cardtype == 'number':
                     top_card = temp_card
-                    turn = 'Player'
+                    turn = getNextPlayer(player, direction)
                 else:
                     if temp_card.rank == 'Skip':
-                        turn = 'Pc'
+                        turn = getNextPlayer(player, direction, skip=True)
                         top_card = temp_card
                     elif temp_card.rank == 'Reverse':
-                        turn = 'Pc'
+                        direction *= -1
+                        turn = getNextPlayer(player, direction)
                         top_card = temp_card
                     elif temp_card.rank == 'Draw2':
-                        player_hand.add_card(deck.deal())
-                        player_hand.add_card(deck.deal())
+                        next_player = getNextPlayer(player, direction)
+                        hands[next_player].add_card(deck.deal())
+                        hands[next_player].add_card(deck.deal())
                         top_card = temp_card
-                        turn = 'Player'
+                        turn = getNextPlayer(player, direction)
                     elif temp_card.rank == 'Draw4':
+                        next_player = getNextPlayer(player, direction)
                         for i in range(4):
-                            player_hand.add_card(deck.deal())
+                            hands[next_player].add_card(deck.deal())
                         top_card = temp_card
-                        draw4color = pc_hand.cards[0].color
-                        if pc_hand.cards[0].color is None:
-                            pc_hand.cards[0].color = random.choice(color)
+                        draw4color = hands[player].cards[0].color
+                        if hands[player].cards[0].color is None:
+                            hands[player].cards[0].color = random.choice(color)
                         print('Color changes to', draw4color)
                         top_card.color = draw4color
-                        turn = 'Player'
+                        turn = getNextPlayer(player, direction)
                     elif temp_card.rank == 'Wild':
                         top_card = temp_card
-                        wildcolor = pc_hand.cards[0].color
-                        if pc_hand.cards[0].color is None:
-                            pc_hand.cards[0].color = random.choice(color)
+                        wildcolor = hands[player].cards[0].color
+                        if hands[player].cards[0].color is None:
+                            hands[player].cards[0].color = random.choice(color)
                         print("Color changes to", wildcolor)
                         top_card.color = wildcolor
-                        turn = 'Player'
+                        turn = getNextPlayer(player, direction)
             else:
                 print('\nPC pulls a card from deck')
                 time.sleep(1)
@@ -292,44 +300,46 @@ while True:
                     time.sleep(1)
                     if temp_card.cardtype == 'number':
                         top_card = temp_card
-                        turn = 'Player'
+                        turn = getNextPlayer(player, direction)
                     else:
                         if temp_card.rank == 'Skip':
-                            turn = 'Pc'
+                            turn = getNextPlayer(player, direction, skip=True)
                             top_card = temp_card
                         elif temp_card.rank == 'Reverse':
-                            turn = 'Pc'
+                            direction *= -1
+                            turn = getNextPlayer(player, direction)
                             top_card = temp_card
                         elif temp_card.rank == 'Draw2':
-                            player_hand.add_card(deck.deal())
-                            player_hand.add_card(deck.deal())
+                            next_player = getNextPlayer(player, direction)
+                            hands[next_player].add_card(deck.deal())
+                            hands[next_player].add_card(deck.deal())
                             top_card = temp_card
-                            turn = 'Player'
+                            turn = getNextPlayer(player, direction)
                         elif temp_card.rank == 'Draw4':
+                            next_player = getNextPlayer(player, direction)
                             for i in range(4):
-                                player_hand.add_card(deck.deal())
+                                hands[next_player].add_card(deck.deal())
                             top_card = temp_card
-                            draw4color = pc_hand.cards[0].color
+                            draw4color = hands[player].cards[0].color
                             print('Color changes to', draw4color)
                             top_card.color = draw4color
-                            turn = 'Player'
+                            turn = getNextPlayer(player, direction)
                         elif temp_card.rank == 'Wild':
                             top_card = temp_card
-                            wildcolor = pc_hand.cards[0].color
+                            wildcolor = hands[player].cards[0].color
                             print('Color changes to', wildcolor)
                             top_card.color = wildcolor
-                            turn = 'Player'
+                            turn = getNextPlayer(player, direction)
                 else:
-                    print('PC doesnt have a card')
+                    print('PC doesn\'t have a card')
                     time.sleep(1)
-                    pc_hand.add_card(temp_card)
-                    turn = 'Player'
-            print('\nPC has {} cards remaining'.format(pc_hand.no_of_cards()))
-            if pc_hand.no_of_cards() == 1:
+                    hands[player].add_card(temp_card)
+                    turn = getNextPlayer(player, direction)
+            print('\nPC has {} cards remaining'.format(hands[player].no_of_cards()))
+            if hands[player].no_of_cards() == 1:
                 print('PC Yells UNO!!')
-            print('Player has {} cards remaining'.format(player_hand.no_of_cards()))
             time.sleep(1)
-            if win_check(pc_hand):
+            if win_check(hands[player]):
                 print('\nPC WON!!')
                 playing = False
 
